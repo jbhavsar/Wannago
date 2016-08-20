aaac03a1e5c2090af12cb0a938a31622293feda9
Jay Bhavsar <BlackHat@jays-macbook-pro.local>
2016-08-20 02:52:17 -0400

    Initial Commit

---
+++ wannago/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wannago
+//
+//  Created by Jay Bhavsar on 2016-08-20.
+//  Copyright © 2016 zeroxp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
